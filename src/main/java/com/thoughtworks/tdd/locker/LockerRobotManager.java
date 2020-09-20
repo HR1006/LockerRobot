0e7e575987ca7414cc767c5f590f39d8e4b8bed3
Hairui guo <hairui.guo@thoughtworks.com>
2020-09-20 16:20:30 +0800

    [hairui] should_return_ticket_when_deposit_bag_given_one_S_bag_one_S_locker_with_free_capacity

---
+++ src/main/java/com/thoughtworks/tdd/locker/LockerRobotManager.java
@@ -0,0 +1,42 @@
+package com.thoughtworks.tdd.locker;
+
+import com.thoughtworks.tdd.locker.exception.LockerFullException;
+import com.thoughtworks.tdd.locker.storeable.Locker;
+import com.thoughtworks.tdd.locker.storeable.Storeable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LockerRobotManager {
+    private final List<Storeable> storeables = new ArrayList<>();
+
+    public void addStoreable(Storeable storeable) {
+        storeables.add(storeable);
+    }
+
+    public Ticket depositBagByLocker(Bag bag) {
+        Ticket ticket = null;
+        List<Locker> lockers = storeables
+                .stream()
+                .filter(storeable -> storeable instanceof Locker)
+                .map(storeable -> (Locker) storeable)
+                .collect(Collectors.toList());
+        for (Locker locker : lockers) {
+            if (locker.freeCapacity() > 0) {
+                ticket = locker.depositBag(bag);
+                break;
+            }
+        }
+        return ticket;
+    }
+
+    public Ticket depositBag(Bag bag) {
+        Ticket ticket;
+        ticket = depositBagByLocker(bag);
+        if (ticket != null) {
+            return ticket;
+        }
+        throw new LockerFullException();
+    }
+}
---
+++ src/test/java/com/thoughtworks/tdd/locker/LockerRobotManagerTest.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.tdd.locker;
+
+import com.thoughtworks.tdd.locker.storeable.Locker;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LockerRobotManagerTest {
+
+    private Locker initLocker(String lockerSize, int totalCapacity, int freeCapacity) {
+        Locker locker = new Locker(lockerSize, totalCapacity);
+        for (int i = 0; i < totalCapacity - freeCapacity; i++) {
+            locker.depositBag(new Bag(lockerSize));
+        }
+        return locker;
+    }
+
+    @Test
+    public void should_return_ticket_when_deposit_bag_given_one_S_bag_one_S_locker_with_free_capacity() {
+        LockerRobotManager manager = new LockerRobotManager();
+        Locker locker = initLocker(Constants.SIZE_S, 1, 1);
+        manager.addStoreable(locker);
+        Bag bag = new Bag(Constants.SIZE_S);
+        Ticket ticket =manager.depositBag(bag);
+        Bag result = locker.pickUpBag(ticket);
+        Assert.assertEquals(result, bag);
+    }
+}
